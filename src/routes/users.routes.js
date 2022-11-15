16971ab88a644aafb45d782c3ec7fcaec3abacba
Vanessa Pereira <vnspereira11@gmail.com>
2022-11-14 22:51:30 -0300

    SQLite connection, users schema, create and update users

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,5 +4,6 @@ const usersRouter = Router();
 const usersController = new UsersController()
 
 usersRouter.post("/", usersController.create);
+usersRouter.put("/:id", usersController.update);
 
 module.exports = usersRouter;
