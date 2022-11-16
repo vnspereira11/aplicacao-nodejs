c141adfb599b14802d48d0afdb21c379681f6b92
Vanessa Pereira <vnspereira11@gmail.com>
2022-11-16 15:11:14 -0300

    Created movie_notes and movie_tags schema

---
+++ src/database/knex/migrations/20221116175034_createMovieTags.js
@@ -0,0 +1,8 @@
+exports.up = knex => knex.schema.createTable("movie_tags", table => {
+  table.increments("id");
+  table.string("name");
+  table.integer("note_id").references("id").inTable("movie_notes").onDelete("CASCADE");
+  table.integer("user_id").references("id").inTable("users");  
+})
+
+exports.down = knex => knex.schema.dropTable("movie_tags");
